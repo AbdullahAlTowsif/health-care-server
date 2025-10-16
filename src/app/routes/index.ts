81fb0dc216afc3fd23a740c9df806ef15428a432
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-10-17 02:03:04 +0600

    schedule slot ongoing..

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { userRoutes } from '../modules/user/user.routes';
 import { authRoutes } from '../modules/auth/auth.routes';
+import path from 'path';
+import { ScheduleRoutes } from '../modules/schedule/schedule.routes';
 
 
 const router = express.Router();
@@ -13,6 +15,10 @@ const moduleRoutes = [
     {
         path: '/auth',
         route: authRoutes
+    },
+    {
+        path: '/schedule',
+        route: ScheduleRoutes
     },
 ];
 
