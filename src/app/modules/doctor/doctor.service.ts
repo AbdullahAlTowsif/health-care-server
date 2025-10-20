b1117d29169bb52b46025044316bd470d8b4e76f
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-10-20 23:56:36 +0600

    setting up openrouter AI

--- src/app/modules/doctor/doctor.service.ts
+++ src/app/modules/doctor/doctor.service.ts
@@ -3,6 +3,9 @@ import { IOptions, paginationHelper } from "../../helper/paginationHelper";
 import { doctorSearchableFields } from "./doctor.constant";
 import { prisma } from "../../shared/prisma";
 import { IDoctorUpdateInput } from "./doctor.interface";
+import ApiError from "../../errors/ApiError";
+import httpStatus from "http-status";
+import { openai } from "../../helper/openRouter";
 
 const getAllFromDB = async (filters: any, options: IOptions) => {
     const { page, limit, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
@@ -22,7 +25,7 @@ const getAllFromDB = async (filters: any, options: IOptions) => {
         })
     }
 
-    if(specialties && specialties.length > 0) {
+    if (specialties && specialties.length > 0) {
         andConditions.push({
             doctorSpecialties: {
                 some: {
@@ -135,7 +138,59 @@ const updateIntoDB = async (id: string, payload: Partial<IDoctorUpdateInput>) =>
 
 }
 
+
+const getAISuggestions = async (payload: { symptoms: string }) => {
+    if (!(payload && payload.symptoms)) {
+        throw new ApiError(httpStatus.BAD_REQUEST, "Symptom is required")
+    };
+
+    const doctors = await prisma.doctor.findMany({
+        where: {
+            isDeleted: false
+        },
+        include: {
+            doctorSpecialties: {
+                include: {
+                    specialities: true
+                }
+            }
+        }
+    });
+
+
+    const prompt = `
+    You are a medical assistant AI. Based on the patient's symptoms, suggest the top 3 most suitable doctors.
+    Each doctor has specialties and years of experience.
+    Only suggest doctors who are relevant to the given symptoms.
+
+    Symptoms: ${payload.symptoms}
+
+    Here is the doctor list (in JSON):
+    ${JSON.stringify(doctors, null, 2)}
+
+    Return your response in JSON format with full individual doctor data. 
+    `;
+
+
+    const completion = await openai.chat.completions.create({
+        model: 'z-ai/glm-4.5-air:free',
+        messages: [
+            {
+                role: "system",
+                content:
+                    "You are a helpful AI medical assistant that provides doctor suggestions.",
+            },
+            {
+                role: 'user',
+                content: prompt,
+            },
+        ],
+    });
+    console.log(doctors);
+}
+
 export const DoctorService = {
     getAllFromDB,
-    updateIntoDB
+    updateIntoDB,
+    getAISuggestions
 }
