32e5c8c62dbd52e874af5cf3d1963190344663f8
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-10-23 16:54:14 +0600

    payment module and webhook added for stripe

---
+++ src/app/modules/payment/payment.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import { PaymentService } from "./payment.service";
+import sendResponse from "../../shared/sendResponse";
+import { stripe } from "../../helper/stripe";
+
+const handleStripeWebhookEvent = catchAsync(async (req: Request, res: Response) => {
+
+    const sig = req.headers["stripe-signature"] as string;
+    const webhookSecret = "whsec_0aa8cab151e34cd0f2a1be65183de8a67590fe7c19309c92bbb4937b2aa2053f"
+
+    let event;
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (err: any) {
+        console.error("⚠️ Webhook signature verification failed:", err.message);
+        return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
+    const result = await PaymentService.handleStripeWebhookEvent(event);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: 'Webhook req send successfully',
+        data: result,
+    });
+});
+
+export const PaymentController = {
+    handleStripeWebhookEvent
+}
