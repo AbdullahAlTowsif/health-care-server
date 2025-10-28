3e7bc85f77b46ff15c8ff95057bcfd61989a1f9c
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-10-28 23:11:02 +0600

    doctor schedule module completed

--- src/app/modules/doctorSchedule/doctorSchedule.routes.ts
+++ src/app/modules/doctorSchedule/doctorSchedule.routes.ts
@@ -7,11 +7,31 @@ import validateRequest from "../../middlewares/validateRequest";
 
 const router = express.Router();
 
+
+router.get(
+    '/',
+    auth(UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
+    DoctorScheduleController.getAllFromDB
+);
+
+router.get(
+    '/my-schedule',
+    auth(UserRole.DOCTOR),
+    DoctorScheduleController.getMySchedule
+)
+
 router.post(
     "/",
     auth(UserRole.DOCTOR),
     validateRequest(DoctorScheduleValidation.createDoctorScheduleZodValidationSchema),
     DoctorScheduleController.insertIntoDB
 )
+
+
+router.delete(
+    '/:id',
+    auth(UserRole.DOCTOR),
+    DoctorScheduleController.deleteFromDB
+);
 
 export const doctorScheduleRoutes = router;
