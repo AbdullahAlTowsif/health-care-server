6cdee8e41e42f4b156d52adc53e00b21e343dbf3
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-12-15 03:59:51 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.post(
 
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://health-care-client-lake.vercel.app'],
     credentials: true
 }));
 
